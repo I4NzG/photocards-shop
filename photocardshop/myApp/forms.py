178efe5f18feeae030cba3e6bc77ef66e4736bd7
Ian Zepeda <ianzepeda2004@gmail.com>
2025-05-07 16:37:19 -0700

    Add email field to UserRegisterForm for enhanced user registration

--- photocardshop/myApp/forms.py
+++ photocardshop/myApp/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
